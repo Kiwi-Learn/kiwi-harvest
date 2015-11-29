0f7305ed7b9f123f0437a000d354d2257e4984b6
Che-Wei Lin <linton.tw@gmail.com>
2015-11-29 17:32:58 +0800

    add more test case

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -12,7 +12,18 @@ describe 'Prognition Stories' do
 
   describe 'Visiting the home page' do
     it 'finds the title' do
-      @browser.title.must_equal 'Kiwi harvest courses information'
+      @browser.title.must_equal 'Kiwi harvest'
+    end
+  end
+
+  describe 'List all courses' do
+    it 'check courses list page and table' do
+      @browser.link(text: 'Courses').click
+
+      @browser.url.must_match %r{http.*/courses}
+
+      @browser.table(class: 'table').rows.count.must_be :>=, 100
+      @browser.table(class: 'table').rows[0].text.must_match(/Course ID/)
     end
   end
 
