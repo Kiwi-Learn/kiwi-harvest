e2133f954bd78ef4771fae0d0e7eb78469aa65b4
Che-Wei Lin <linton.tw@gmail.com>
2015-12-06 21:05:52 +0800

    Statistics views and controllers

--- controllers/application_controller.rb
+++ controllers/application_controller.rb
@@ -78,9 +78,12 @@ class ApplicationController < Sinatra::Base
   end
 
   app_get_statistics = lambda do
-    @chart_data = {'JAN' => 1, 'FEB' => 2, 'MAR' => 3, 'APR' => 4, 'MAY' => 5,
-      'JUN' => 6, 'JUL' => 7, 'AUG' => 8, 'SEP' => 9, 'OCT' => 10, 'NOV' => 11,
-      'DEC' => 12}
+    request_url = "#{settings.api_server}/#{settings.api_ver}/courselist"
+    results = GetCoursesDateStatisticsAPI.new(request_url).call
+    @chart_data_2015 = results.twenty_fifteen
+    @chart_data_2014 = results.twenty_fourteen
+    @chart_data_2013 = results.twenty_thirteen
+    @chart_data_2012 = results.twenty_twelve
     slim :statistics
   end
 
